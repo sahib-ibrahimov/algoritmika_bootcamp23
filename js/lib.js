b4c7976fa37a3467d7fca4776b3191d0061d8848
sahib-ibrahimov <sahib.ibrahimov1998@gmail.com>
2022-10-18 11:32:03 +0400

    random

--- js/lib.js
+++ js/lib.js
@@ -5,3 +5,11 @@ function print(obj) {
 function debug(obj) {
   console.warn(obj);
 }
+
+const isAlfa = ch => (('a' <= ch) && (ch <= 'z')) || (('A' <= ch) && (ch <= 'Z'));
+const isDigit = ch => ('0' <= ch) && (ch <= '9');
+
+const even = n => !(n % 2);
+const odd = n => n % 2;
+
+const random = (max=100, min=0) => Math.trunc( Math.random() * (max-min) + min);
