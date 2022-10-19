ec71da1443770f1c15f684ecc60278e535659a18
sahib-ibrahimov <sahib.ibrahimov1998@gmail.com>
2022-10-19 12:01:11 +0400

    isNumber

--- js/lib.js
+++ js/lib.js
@@ -2,7 +2,7 @@ function print(obj) {
   console.log(obj);
 }
 
-function debug(obj) {
+function debug(obj=' ') {
   console.warn(obj);
 }
 
@@ -13,3 +13,17 @@ const even = n => !(n % 2);
 const odd = n => n % 2;
 
 const random = (max=100, min=0) => Math.trunc( Math.random() * (max-min) + min);
+
+function count(str, ch) {
+  let cem = 0;
+  for(let i=0; i<str.length; ++i)
+    if(str[i] === ch) ++cem;
+  return cem;
+}
+
+function isNumber(str) {
+  for(let i=0; i<str.length; ++i)
+    if( !isDigit(str[i]) )
+      return false;
+  return true;
+}
